120a52e073708ff992629584b309f7a18a999e73
TornikeL38 <120584873+TornikeL38@users.noreply.github.com>
2023-01-29 17:02:19 +0400

    add js

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+let number = "3"
+let number2 = "8"
+let number3 = "11"
+let number4 = "15"
+let number5 = "27"
+let numbers = [number, number2, number3, number4, number5]
+console.log (number + number2 + number3)
+
+
+let personsName = [
+    {
+        name: "Tornike",
+        age: "21",
+        address: "street3",
+    },
+    {
+        name: "Magda",
+        age: "22",
+        address: "street2"
+    },
+    {
+        name: "Lasha",
+        age: "20",
+        address: "street1"
+    }
+]
+console.log(personsName[0].name)
+console.log(`My age is ${personsName[1].age}`)
+// cpnsole.log(`My address is ${personsName[2].address}`)
